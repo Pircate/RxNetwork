9ca25391de0b98a9033f6e25197c77e8c0cfc149
高翔 <gao497868860@163.com>
2018-04-19 12:49:41 +0800

    Commit: update podspec

--- Example/RxNetwork/KeyedDecodingContainer+TypeConversion.swift
+++ Example/RxNetwork/KeyedDecodingContainer+TypeConversion.swift
@@ -9,23 +9,23 @@
 import Foundation
 
 extension KeyedDecodingContainer {
-//    public func decodeIfPresent(_ type: String.Type, forKey key: K) throws -> String? {
-//        if let value = try? decode(type, forKey: key) {
-//            return value
-//        }
-//        if let intValue = try? decode(Int.self, forKey: key){
-//            return String(intValue)
-//        }
-//        if let doubleValue = try? decode(Double.self, forKey: key) {
-//            return String(doubleValue)
-//        }
-//        if let floatValue = try? decode(Float.self, forKey: key) {
-//            return String(floatValue)
-//        }
-//        return nil
-//    }
-//    
-//    public func decodeIfPresent<T>(_ type: T.Type, forKey key: K) throws -> T? where T : Decodable {
-//        return try? decode(type, forKey: key)
-//    }
+    public func decodeIfPresent(_ type: String.Type, forKey key: K) throws -> String? {
+        if let value = try? decode(type, forKey: key) {
+            return value
+        }
+        if let intValue = try? decode(Int.self, forKey: key){
+            return String(intValue)
+        }
+        if let doubleValue = try? decode(Double.self, forKey: key) {
+            return String(doubleValue)
+        }
+        if let floatValue = try? decode(Float.self, forKey: key) {
+            return String(floatValue)
+        }
+        return nil
+    }
+    
+    public func decodeIfPresent<T>(_ type: T.Type, forKey key: K) throws -> T? where T : Decodable {
+        return try? decode(type, forKey: key)
+    }
 }
--- RxNetwork.podspec
+++ RxNetwork.podspec
@@ -9,7 +9,7 @@
 Pod::Spec.new do |s|
   s.name             = 'RxNetwork'
   s.version          = '0.1.0'
-  s.summary          = 'A short description of RxNetwork.'
+  s.summary          = 'A swift network library.'
 
 # This description is used to generate tags and improve search results.
 #   * Think: What does it do? Why did you write it? What is the focus?
@@ -40,4 +40,7 @@ TODO: Add long description of the pod here.
   # s.frameworks = 'UIKit', 'MapKit'
   s.dependency 'Moya/RxSwift'
   s.dependency 'Cache'
+  
+  s.requires_arc = true
+  s.swift_version = '4.0'
 end
