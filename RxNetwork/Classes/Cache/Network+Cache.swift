d9ed73034c1c23945297ebd2e7fc38c68557745b
Pircate <gao497868860@163.com>
2018-07-08 01:16:19 +0800

    code optimization

--- RxNetwork/Classes/Cache/Network+Cache.swift
+++ RxNetwork/Classes/Cache/Network+Cache.swift
@@ -27,24 +27,15 @@ extension Network {
 public extension Network.Cache {
     
     func cachedObject<C: Codable>(_ type: C.Type, for target: TargetType) throws -> C {
-        let storage = try Storage(diskConfig: DiskConfig(name: kNetworkObjectCacheName),
-                                  memoryConfig: MemoryConfig(),
-                                  transformer: TransformerFactory.forCodable(ofType: type))
-        return try storage.object(forKey: target.cachedKey)
+        return try Storage<C>().object(forKey: target.cachedKey)
     }
     
     func storeCachedObject<C: Codable>(_ cachedObject: C, for target: TargetType) throws {
-        let storage = try Storage(diskConfig: DiskConfig(name: kNetworkObjectCacheName),
-                                  memoryConfig: MemoryConfig(),
-                                  transformer: TransformerFactory.forCodable(ofType: C.self))
-        try storage.setObject(cachedObject, forKey: target.cachedKey)
+        try Storage<C>().setObject(cachedObject, forKey: target.cachedKey)
     }
     
     func removeCachedObject<C: Codable>(_ type: C.Type, for target: TargetType) throws {
-        let storage = try Storage(diskConfig: DiskConfig(name: kNetworkObjectCacheName),
-                                  memoryConfig: MemoryConfig(),
-                                  transformer: TransformerFactory.forCodable(ofType: type))
-        try storage.removeObject(forKey: target.cachedKey)
+        try Storage<C>().removeObject(forKey: target.cachedKey)
     }
     
     func removeAllCachedObjects() throws {
@@ -55,38 +46,38 @@ public extension Network.Cache {
 public extension Network.Cache {
     
     func cachedResponse(for target: TargetType) throws -> Response {
-        let storage = try Storage(diskConfig: DiskConfig(name: kNetworkResponseCacheName),
-                                  memoryConfig: MemoryConfig(),
-                                  transformer: Transformer<Response>(
-                                    toData: { $0.data },
-                                    fromData: { Response(statusCode: 200, data: $0) }))
-        return try storage.object(forKey: target.cachedKey)
+        return try Storage<Response>().object(forKey: target.cachedKey)
     }
     
     func storeCachedResponse(_ cachedResponse: Response, for target: TargetType) throws {
-        let storage = try Storage(diskConfig: DiskConfig(name: kNetworkResponseCacheName),
-                                  memoryConfig: MemoryConfig(),
-                                  transformer: Transformer<Response>(
-                                    toData: { $0.data },
-                                    fromData: { Response(statusCode: 200, data: $0) }))
-        try storage.setObject(cachedResponse, forKey: target.cachedKey)
+        try Storage<Response>().setObject(cachedResponse, forKey: target.cachedKey)
     }
     
     func removeCachedResponse(for target: TargetType) throws {
-        let storage = try Storage(diskConfig: DiskConfig(name: kNetworkResponseCacheName),
-                                  memoryConfig: MemoryConfig(),
-                                  transformer: Transformer<Response>(
-                                    toData: { $0.data },
-                                    fromData: { Response(statusCode: 200, data: $0) }))
-        try storage.removeObject(forKey: target.cachedKey)
+        try Storage<Response>().removeObject(forKey: target.cachedKey)
     }
     
     func removeAllCachedResponses() throws {
-        let storage = try Storage(diskConfig: DiskConfig(name: kNetworkResponseCacheName),
-                                  memoryConfig: MemoryConfig(),
-                                  transformer: Transformer<Response>(
-                                    toData: { $0.data },
-                                    fromData: { Response(statusCode: 200, data: $0) }))
-        try storage.removeAll()
+        try Storage<Response>().removeAll()
+    }
+}
+
+fileprivate extension Storage where T: Codable {
+    
+    convenience init() throws {
+        try self.init(diskConfig: DiskConfig(name: kNetworkObjectCacheName),
+                      memoryConfig: MemoryConfig(),
+                      transformer: TransformerFactory.forCodable(ofType: T.self))
+    }
+}
+
+fileprivate extension Storage where T == Response {
+    
+    convenience init() throws {
+       try self.init(diskConfig: DiskConfig(name: kNetworkResponseCacheName),
+                     memoryConfig: MemoryConfig(),
+                     transformer: Transformer<Response>(
+                        toData: { $0.data },
+                        fromData: { Response(statusCode: 200, data: $0) }))
     }
 }
